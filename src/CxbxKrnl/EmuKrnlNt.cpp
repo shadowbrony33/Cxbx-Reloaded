127ec9e46fbfb1b770794dbdcd0bfa0617d1e38e
0x7FFFFFFF <shadowbrony33@gmail.com>
2018-04-18 10:17:29 -0600

    Revert "Testing Hack for Conker: Live and Reloaded"

--- src/CxbxKrnl/EmuKrnlNt.cpp
+++ src/CxbxKrnl/EmuKrnlNt.cpp
@@ -1413,9 +1413,6 @@ XBSYSAPI EXPORTNUM(217) xboxkrnl::NTSTATUS NTAPI xboxkrnl::NtQueryVirtualMemory
 	}
 
 	NTSTATUS ret = g_VMManager.XbVirtualMemoryStatistics((VAddr)BaseAddress, Buffer);
-	
-	// Hack for Conker Live and Reloaded
-	ret = STATUS_TIMEOUT;
 
 	if (ret == STATUS_SUCCESS)
 	{
